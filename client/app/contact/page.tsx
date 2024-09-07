b68ad69d9b2d1fdc47cacacd1a164c810365e316
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-07 13:50:10 +0530

    contact page

--- client/app/contact/page.tsx
+++ client/app/contact/page.tsx
@@ -1,11 +1,179 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const page = () => {
+const ContactSection = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [issue, SetIssue] = useState('');
+
+
+
+  const notify = async () => {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/submitissues`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, issue }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to take your request ');
+      }
+      toast.success('Issue sent successfully!', {
+        theme: 'dark',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        position: 'top-right'
+      });
+    } catch (error) {
+      console.error('Failed to add comment', error);
+      toast.error('Place Try again!', {
+        theme: 'dark',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        position: 'top-right'
+      });
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className=" dark:bg-slate-800" id="contact">
+      <ToastContainer />
+      <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
+        <div className="mb-4">
+          <div className="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12 ">
+            <h2 className="font-heading mb-4 font-bold tracking-tight text-white dark:text-white text-3xl sm:text-5xl">
+              Get in Touch
+            </h2>
+            <p className="mx-auto mt-4 max-w-3xl text-xl  text-white dark:text-slate-400">In hac habitasse platea
+              dictumst
+            </p>
+          </div>
+        </div>
+        <div className="flex items-stretch justify-center">
+          <div className="grid md:grid-cols-2">
+            <div className="h-full pr-6">
+              <p className="mt-3 mb-12 text-lg text-white dark:text-slate-400">
+                Class aptent taciti sociosqu ad
+                litora torquent per conubia nostra, per inceptos himenaeos. Duis nec ipsum orci. Ut scelerisque
+                sagittis ante, ac tincidunt sem venenatis ut.
+              </p>
+              <ul className="mb-6 md:mb-0">
+                <li className="flex">
+                  <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                      fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                      strokeLinejoin="round" className="h-6 w-6">
+                      <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+                      <path
+                        d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z">
+                      </path>
+                    </svg>
+                  </div>
+                  <div className="ml-4 mb-4">
+                    <h3 className="mb-2 text-lg font-medium leading-6 text-white dark:text-white">Our Address
+                    </h3>
+                    <p className="text-white dark:text-slate-400">1230 Maecenas Street Donec Road</p>
+                    <p className="text-white dark:text-slate-400">New York, EEUU</p>
+                  </div>
+                </li>
+                <li className="flex">
+                  <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                      fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                      strokeLinejoin="round" className="h-6 w-6">
+                      <path
+                        d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2">
+                      </path>
+                      <path d="M15 7a2 2 0 0 1 2 2"></path>
+                      <path d="M15 3a6 6 0 0 1 6 6"></path>
+                    </svg>
+                  </div>
+                  <div className="ml-4 mb-4">
+                    <h3 className="mb-2 text-lg font-medium leading-6 text-white dark:text-white">Contact
+                    </h3>
+                    <p className="text-white dark:text-slate-400">Mobile: +1 (123) 456-7890</p>
+                    <p className="text-white dark:text-slate-400">Mail: tailnext@gmail.com</p>
+                  </div>
+                </li>
+                <li className="flex">
+                  <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                      fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                      strokeLinejoin="round" className="h-6 w-6">
+                      <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+                      <path d="M12 7v5l3 3"></path>
+                    </svg>
+                  </div>
+                  <div className="ml-4 mb-4">
+                    <h3 className="mb-2 text-lg font-medium leading-6 text-white dark:text-white">Working
+                      hours</h3>
+                    <p className="text-white dark:text-slate-400">Monday - Friday: 08:00 - 17:00</p>
+                    <p className="text-white dark:text-slate-400">Saturday &amp; Sunday: 08:00 - 12:00</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="card h-fit max-w-6xl p-5 md:p-12" id="form">
+              <h2 className="mb-4 text-2xl font-bold">Ready to Get Started?</h2>
+              <form id="contactForm" onSubmit={(e) => {
+                e.preventDefault();
+                notify();
+              }}>
+                <div className="mb-6">
+                  <div className="mx-0 mb-1 sm:mb-4">
+                    <div className="mx-0 mb-1 sm:mb-4">
+                      <label htmlFor="name" className="pb-1 text-xs uppercase tracking-wider"></label>
+                      <input type="text" id="name" autoComplete="given-name" placeholder="Your name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                        className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+                        name="name" />
+                    </div>
+                    <div className="mx-0 mb-1 sm:mb-4">
+                      <label htmlFor="email" className="pb-1 text-xs uppercase tracking-wider"></label>
+                      <input type="email" id="email" autoComplete="email" placeholder="Your email address"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+                        name="email" />
+                    </div>
+                  </div>
+                  <div className="mx-0 mb-1 sm:mb-4">
+                    <label htmlFor="textarea" className="pb-1 text-xs uppercase tracking-wider"></label>
+                    <textarea id="textarea" name="textarea" placeholder="Write your message..."
+                      value={issue}
+                      onChange={(e) => SetIssue(e.target.value)}
+                      required
+                      className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"></textarea>
+                  </div>
+                </div>
+                <div className="text-center">
+                  <button type="submit"
+                    className="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send Message</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default ContactSection;
+
