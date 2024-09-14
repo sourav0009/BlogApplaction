96ab67cc3642da4af643518d73609bd2a85e56ab
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-14 17:52:46 +0530

    dashboard

---
+++ client/app/dashboard/comments/page.jsx
@@ -0,0 +1,113 @@
+"use client";
+import { useEffect, useState, useCallback } from "react";
+import cookie from "js-cookie";
+import { useRouter } from "next/navigation";
+
+const Comment = () => {
+    const [comments, setComments] = useState([]);
+    const [ setLoading] = useState(false);
+    const [authorEmail, setAuthorEmail] = useState("");
+    const token = cookie.get("cookie-1");
+    const router = useRouter();
+
+    const validation = useCallback(async () => {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/verifyjwt`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ token: token }),
+            });
+            if (response.status === 200) {
+                console.log("User loggedin");
+            } else {
+                console.log(
+                    "You are not eligible to access this route ! Please login first"
+                );
+                router.push("/login");
+            }
+        } catch (error) {
+            console.error("Server error autologin failed", error);
+        }
+    }, [token, router]);
+
+    const fetchEmail = useCallback(async () => {
+        try {
+            const fetchedEmail = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/getuser`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ token }),
+            });
+            const userData = await fetchedEmail.json();
+            const email = userData.username.email;
+            setAuthorEmail(email);
+        } catch (error) {
+            console.log("failed to fetch Email", error);
+        }
+    }, [token]);
+
+    const fetchComments = useCallback(async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/getcomments`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ authorEmail }),
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch comments");
+            }
+
+            const data = await response.json();
+            setComments(data);
+        } catch (error) {
+            console.error("Failed to fetch comments", error);
+        }
+    }, [authorEmail, setLoading]);
+    useEffect(() => {
+        validation();
+        fetchEmail();
+        fetchComments();
+    }, [validation, fetchEmail, fetchComments]);
+
+    return (
+        <div className="mr-16 ml-80 mt-20">
+            <ol className="relative border-s border-gray-200 dark:border-gray-700">
+                {comments.length > 0 ? (
+                    comments.map((item, index) => (
+                        <li key={index}>
+                            <span className=" absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"></span>
+                            <h2 className="pb-7 pl-6  cursor-pointer text-yellow-400 hover:text-blue-500">
+                                {item.query[0].query}
+                            </h2>
+                            <ul>
+                                {item.descriptions.map((description, idx) => (
+                                    <li className="mb-8 ms-6" key={idx}>
+                                        <div className="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
+                                            <time className="mb-1 text-xs font-normal text-lime-500 sm:order-last sm:mb-0">
+                                                {description.timestamp}
+                                            </time>
+                                            <div className="text-sm font-normal text-gray-500 dark:text-gray-300">
+                                                {description.description}
+                                            </div>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))
+                ) : (
+                    <h1 className="font-extrabold text-center">No comments posted</h1>
+                )}
+            </ol>
+        </div>
+    );
+};
+
+export default Comment;
---
+++ client/app/dashboard/index/page.jsx
@@ -0,0 +1,83 @@
+"use client";
+import { useState, useEffect,useCallback } from "react";
+import cookie from "js-cookie";
+import "react-toastify/dist/ReactToastify.css";
+
+const Inbox = () => {
+  const [senderEmail, setSenderEmail] = useState("");
+  const [, setAuthorName] = useState("");
+  const [inboxes, setInboxes] = useState("");
+  const token = cookie.get("cookie-1");
+
+  const fetchEmailInboxes = useCallback( async () => {
+    try {
+      const fetchedEmail = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/getuser`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token }),
+      });
+      const userData = await fetchedEmail.json();
+
+      if (userData && userData.username) {
+        const email = userData.username.email;
+        const name = userData.username.username;
+        setAuthorName(name);
+        setSenderEmail(email);
+      } else {
+        throw new Error("Failed to fetch user data or username is undefined");
+      }
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/getInboxes`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ senderEmail }),
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch Inboxes");
+        }
+        const data = await response.json();
+        setInboxes(data);
+      } catch (error) {
+        console.log("failed to fetch email", error);
+      }
+    } catch (error) {
+      console.log("failed to fetch your inboxes");
+    }
+  },[token,senderEmail]);
+
+  useEffect(() => {
+    fetchEmailInboxes();
+  }, [fetchEmailInboxes]);
+
+  return (
+    <>
+      <div className="mt-28 ml-80 mr-28">
+        {inboxes && inboxes.length > 0 ? (
+          inboxes.map((inbox, index) => (
+            <div key={index} className="mb-5 border-b-2 border-white pb-6">
+              <p className="text-blue-500 font-bold text-xl">{inbox.query}</p>
+              <div className="mt-4">
+                {inbox.description.map((desc, descIndex) => (
+                  <div key={descIndex} className="mb-2 text-gray-400">
+                    <p>{desc}</p>
+                    <p className="text-right text-yellow-300 ml-4">
+                      commented by- {inbox.authorName[descIndex]}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No Available Query</p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Inbox;
